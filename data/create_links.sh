43795024fc1543e31f76642965b02c945e7a2a66
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-02-27 15:18:35 -0800

    create_links.sh: initial import

---
+++ data/create_links.sh
@@ -0,0 +1,63 @@
+
+
+
+BDIR="/gdata/atlas/gerbaudo/md5/runs"
+
+mkdir -p data/sm_zbbar_mm_take1
+cd data/sm_zbbar_mm_take1
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r00/tag_1_delphes_events.root  00.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r01/tag_1_delphes_events.root  01.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r02/tag_1_delphes_events.root  02.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r03/tag_1_delphes_events.root  03.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r04/tag_1_delphes_events.root  04.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r05/tag_1_delphes_events.root  05.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r06/tag_1_delphes_events.root  06.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r07/tag_1_delphes_events.root  07.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r08/tag_1_delphes_events.root  08.root
+ln -s ${BDIR}/sm_zbbar_mm_take1/Events/r09/tag_1_delphes_events.root  09.root
+cd -
+
+mkdir -p data/sm_zb_mm_take1
+cd data/sm_zb_mm_take1
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r00/tag_1_delphes_events.root 00.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r01/tag_1_delphes_events.root 01.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r02/tag_1_delphes_events.root 02.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r03/tag_1_delphes_events.root 03.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r04/tag_1_delphes_events.root 04.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r05/tag_1_delphes_events.root 05.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r06/tag_1_delphes_events.root 06.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r07/tag_1_delphes_events.root 07.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r08/tag_1_delphes_events.root 08.root
+ln -s ${BDIR}/sm_zb_mm_take1/Events/r09/tag_1_delphes_events.root 09.root
+cd -
+
+mkdir -p data/bm_zb_mm_take1
+cd data/bm_zb_mm_take1
+ln -s ${BDIR}/bm_zb_mm_take1/Events/r00/tag_1_delphes_events.root    00.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_01/tag_1_delphes_events.root 01.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_02/tag_1_delphes_events.root 02.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_03/tag_1_delphes_events.root 03.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_04/tag_1_delphes_events.root 04.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_05/tag_1_delphes_events.root 05.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_06/tag_1_delphes_events.root 06.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_07/tag_1_delphes_events.root 07.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_08/tag_1_delphes_events.root 08.root
+ln -s ${BDIR}/bm_zb_mm_take1/Events/run_09/tag_1_delphes_events.root 09.root
+cd -
+
+mkdir -p data/bm_zbbar_mm_take1
+cd data/bm_zbbar_mm_take1
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r00/tag_1_delphes_events.root 00.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r01/tag_1_delphes_events.root 01.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r02/tag_1_delphes_events.root 02.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r03/tag_1_delphes_events.root 03.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r04/tag_1_delphes_events.root 04.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r05/tag_1_delphes_events.root 05.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r06/tag_1_delphes_events.root 06.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r07/tag_1_delphes_events.root 07.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r08/tag_1_delphes_events.root 08.root
+ln -s ${BDIR}/bm_zbbar_mm_take1/Events/r09/tag_1_delphes_events.root 09.root
+cd -
+
+
+
